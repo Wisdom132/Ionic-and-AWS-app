8bdc431c9356bee11b16642641a7662ed4f39549
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-27 14:03:56 +0100

    setup project

---
+++ src/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require("express");
+const PORT = process.env.PORT || 4000;
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+
+//--------------registering cors -----------------
+app.use(cors());
+//----------------configure body parser --------------
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+//---------------configure morgan  --------
+app.use(morgan("dev"));
+
+
+// define first route
+app.get("/", (req, res) => {
+    res.json({
+        data: "Hello MEIN developers...The I stands for Ionic😎😎"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running on ${PORT}`);
+});
