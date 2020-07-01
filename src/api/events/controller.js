d09c895a7302299ef1a576ed6cf050c620f1411c
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-07-01 19:38:21 +0100

    added route file

---
+++ src/api/events/controller.js
@@ -0,0 +1,11 @@
+const eventController = require("./controller");
+const router = require("express").Router();
+
+
+router.post("/", eventController.addEvent)
+
+router.get("/", eventController.getAllEvents)
+
+
+
+module.exports = router;
