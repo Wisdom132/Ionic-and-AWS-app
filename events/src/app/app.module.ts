f14ccd4c2997726eba0718a730a3b122b50fe6bc
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-07-05 21:10:06 +0100

    fixed reactive form error

--- events/src/app/app.module.ts
+++ events/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -18,9 +18,9 @@ import { AppComponent } from './app.component';
 		BrowserModule,
 		IonicModule.forRoot(),
 		AppRoutingModule,
-		HttpClientModule,
-		FormsModule,
-		ReactiveFormsModule
+		HttpClientModule
+		// FormsModule,
+		// ReactiveFormsModule
 	],
 	providers: [ StatusBar, SplashScreen, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy } ],
 	bootstrap: [ AppComponent ]
