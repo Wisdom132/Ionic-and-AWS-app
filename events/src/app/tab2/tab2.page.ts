a4c2b68d19ff9abeba04b29fc3c2e6fd897b85d8
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-07-05 14:12:07 +0100

    define form handlers

--- events/src/app/tab2/tab2.page.ts
+++ events/src/app/tab2/tab2.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { EventService } from '../../app/api/event.service';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+	selector: 'app-tab2',
+	templateUrl: 'tab2.page.html',
+	styleUrls: [ 'tab2.page.scss' ]
 })
 export class Tab2Page {
+	fileToUpload: File = null;
 
-  constructor() {}
+	eventForm = new FormGroup({
+		name: new FormControl(''),
+		description: new FormControl('')
+	});
 
+	handleFileInput(files: FileList) {
+		this.fileToUpload = files.item(0);
+		console.log(this.fileToUpload);
+	}
+
+	addEvent() {
+		const formData: FormData = new FormData();
+		formData.append('image', this.fileToUpload, this.fileToUpload.name);
+		formData.append('name', this.eventForm.value.name);
+		formData.append('description', this.eventForm.value.description);
+		this.Event.addEvent(formData).subscribe((data: any) => {
+			console.log(data);
+		});
+	}
+	constructor(private Event: EventService) {}
 }
